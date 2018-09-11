a903e1fa340412b1c8cabe8a2c47a88b6de8c719
harrison-symes <zanelockett@gmail.com>
2018-09-12 11:06:34 +1200

    submit subreddit attached

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
+
+
 const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+  <div>
+      <form onSubmit={ e => {
+    e.preventDefault()
+    dispatch(fetchPosts(document.getElementById('new').value))
+  }}>
+    <input id="new" placeholder ="enter a new subreddit"></input>
+  </form>
+  <button>
     Fetch Posts
   </button>
+  </div>
 )
 
 export default connect()(LoadSubreddit)
